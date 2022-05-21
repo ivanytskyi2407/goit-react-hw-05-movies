5a4b31b4d30e548f9a44ad26e4a51831864e0d74
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-05-21 22:00:32 +0300

    add API

--- src/API/API.js
+++ src/API/API.js
@@ -1,13 +1,64 @@
 import axios from 'axios';
+import { filmName } from './searchMovie';
+import { alertMessage } from './searchAlert';
+import Loading from './loading';
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const KEY = '084ca305e7a4e4bb3dbbc7b67e975385';
 
-const API = async (query, currentPage) => {
-  const { data } = await axios.get(
-    `/?q=${query}&page=${currentPage}&key=25722602-ef4054fc4542d7cb871df6c01&q&image_type=photo&orientation=horizontal&per_page=12`
+export async function getMovies(page) {
+  const response = await axios.get(
+    `${BASE_URL}trending/movie/day?api_key=${KEY}&language=en-US&page=${page}`
   );
-  console.log(data);
-  return data;
-};
+  try {
+    const data = response.data;
 
-export default API;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function fetchSearchMovie(page) {
+  const response = await axios.get(
+    `${BASE_URL}search/movie?api_key=${KEY}&language=en-US&query=${filmName}&page=${page}`
+  );
+  try {
+    const data = response.data;
+
+    alertMessage(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getFilmById(filmId) {
+  try {
+    const axiosFilmId = await axios.get(
+      `${BASE_URL}movie/${filmId}?api_key=${KEY}`
+    );
+
+    return axiosFilmId.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// витянує трейлери для фільма по id
+export async function onfetchTrailers(filmId) {
+  try {
+    const urlTrailers = await axios.get(
+      `${BASE_URL}movie/${filmId}/videos?api_key=${KEY}&language=en-US`
+    );
+    Loading.remove();
+    return urlTrailers.data;
+  } catch (error) {
+    Loading.remove();
+    console.log(error);
+  }
+}
+
+//onfetchTrailers(45274);
+// getMovies();
+// getFilmById();
