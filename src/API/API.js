bf0e8b9d9ee75e706fb989d16adc5b303e3dcd59
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-05-21 22:02:57 +0300

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import { filmName } from './searchMovie';
-import { alertMessage } from './searchAlert';
-import Loading from './loading';
 
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const KEY = '084ca305e7a4e4bb3dbbc7b67e975385';
@@ -12,21 +9,18 @@ export async function getMovies(page) {
   );
   try {
     const data = response.data;
-
     return data;
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function fetchSearchMovie(page) {
+export async function fetchSearchMovie(query, page) {
   const response = await axios.get(
-    `${BASE_URL}search/movie?api_key=${KEY}&language=en-US&query=${filmName}&page=${page}`
+    `${BASE_URL}search/movie?api_key=${KEY}&language=en-US&query=${query}&page=${page}`
   );
   try {
     const data = response.data;
-
-    alertMessage(data);
     return data;
   } catch (error) {
     console.log(error);
@@ -38,27 +32,8 @@ export async function getFilmById(filmId) {
     const axiosFilmId = await axios.get(
       `${BASE_URL}movie/${filmId}?api_key=${KEY}`
     );
-
     return axiosFilmId.data;
   } catch (error) {
     console.log(error);
   }
 }
-
-// витянує трейлери для фільма по id
-export async function onfetchTrailers(filmId) {
-  try {
-    const urlTrailers = await axios.get(
-      `${BASE_URL}movie/${filmId}/videos?api_key=${KEY}&language=en-US`
-    );
-    Loading.remove();
-    return urlTrailers.data;
-  } catch (error) {
-    Loading.remove();
-    console.log(error);
-  }
-}
-
-//onfetchTrailers(45274);
-// getMovies();
-// getFilmById();
