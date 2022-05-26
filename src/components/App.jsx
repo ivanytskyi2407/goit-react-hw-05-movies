e0286e21184f299d5fb40de30fd04cff3cc72c84
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-05-26 16:59:39 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,25 @@
-import AppBar from './AppBar/AppBar.js';
-import Home from './Home/Home';
-import Movies from '../views/Movies/Movies';
-import MovieDetails from '../views/MovieDetails';
-import Cast from '../views/Cast/Cast';
-import Reviews from '../views/Reviews/Reviews';
-import Page404 from './Page404/Page404';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+// import AppBar from './AppBar/AppBar.js';
+// import Home from './Home/Home';
+// import Movies from '../views/Movies/Movies';
+// import MovieDetails from '../views/MovieDetails';
+// import Cast from '../views/Cast/Cast';
+// import Reviews from '../views/Reviews/Reviews';
+// import Page404 from './Page404/Page404';
+
+const AppBar = lazy(() => import('./AppBar/AppBar.js'));
+const Home = lazy(() => import('./Home/Home'));
+const Movies = lazy(() => import('../views/Movies/Movies'));
+const MovieDetails = lazy(() => import('../views/MovieDetails'));
+const Cast = lazy(() => import('../views/Cast/Cast'));
+const Reviews = lazy(() => import('../views/Reviews/Reviews'));
+const Page404 = lazy(() => import('./Page404/Page404'));
 
 export const App = () => {
   return (
-    <>
+    <Suspense fallback={<h1>ЗАГРУЖАЄМО</h1>}>
       <Routes>
         <Route path="/" element={<AppBar />}>
           <Route index exact="true" element={<Home />} />
@@ -21,6 +31,6 @@ export const App = () => {
           <Route path="*" element={<Page404 />} />
         </Route>
       </Routes>
-    </>
+    </Suspense>
   );
 };
